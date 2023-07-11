736a3d891214354d141efd0c5983b85aa23c6f44
kimjisoo1156 <shfur2918@naver.com>
2023-07-12 01:21:02 +0900

    config_front_fail

--- src/main/java/com/example/capstone_1/Config/CORSConfig.java
+++ src/main/java/com/example/capstone_1/Config/CORSConfig.java
@@ -10,7 +10,7 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.219.101:3000") // 프론트 API 도메인 추가
+                .allowedOrigins("") // 프론트 API 도메인 추가
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
