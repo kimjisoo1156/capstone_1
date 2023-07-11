69cccc4bf5d5818f26ff35aa8762681c3fb932b4
kimjisoo1156 <shfur2918@naver.com>
2023-07-12 00:43:22 +0900

    config_front

--- src/main/java/com/example/capstone_1/Capstone1Application.java
+++ src/main/java/com/example/capstone_1/Capstone1Application.java
@@ -2,8 +2,10 @@ package com.example.capstone_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class Capstone1Application {
 
 	public static void main(String[] args) {
