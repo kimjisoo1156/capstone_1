3c140299c246d0448483110fbc7174e9e01626e0
kimjisoo1156 <shfur2918@naver.com>
2023-07-11 18:18:46 +0900

    ymladd_aa

--- src/test/java/com/example/capstone_1/Capstone1ApplicationTests.java
+++ src/test/java/com/example/capstone_1/Capstone1ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.capstone_1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Capstone1ApplicationTests {
 
 	@Test
