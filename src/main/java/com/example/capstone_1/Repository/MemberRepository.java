85e1f2542985b7eb8927f053bc21c3a33b24cf44
kimjisoo1156 <shfur2918@naver.com>
2023-08-06 00:13:28 +0900

    작업옮기기

--- src/main/java/com/example/capstone_1/Repository/MemberRepository.java
+++ src/main/java/com/example/capstone_1/Repository/MemberRepository.java
@@ -1,12 +1,12 @@
-package com.example.capstone_1.Repository;
+package com.example.capstone_1.repository;
 
-import com.example.capstone_1.Entity.MemberEntity;
+
+import com.example.capstone_1.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface MemberRepository extends JpaRepository<MemberEntity, String> {
+import java.util.Optional;
 
-    public MemberEntity findByMemberEmail(String memberEmail);
-
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByEmail(String email); //이메일 찾기 = 아이디 찾기
+    boolean existsByEmail(String email); //이메일 중복 == 아이디 중복
 }
--- src/main/java/com/example/capstone_1/Service/AuthService.java
+++ src/main/java/com/example/capstone_1/Service/AuthService.java
@@ -1,94 +1,88 @@
-package com.example.capstone_1.Service;
+package com.example.capstone_1.service;
 
-import com.example.capstone_1.Dto.ResponseDto;
-import com.example.capstone_1.Dto.SignInDto;
-import com.example.capstone_1.Dto.SignInResponseDto;
-import com.example.capstone_1.Dto.SignUpDto;
-import com.example.capstone_1.Entity.MemberEntity;
-import com.example.capstone_1.Repository.MemberRepository;
-import com.example.capstone_1.Security.TokenProvider;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.example.capstone_1.domain.Member;
+import com.example.capstone_1.domain.RefreshToken;
+import com.example.capstone_1.dto.*;
+import com.example.capstone_1.jwt.TokenProvider;
+import com.example.capstone_1.repository.MemberRepository;
+import com.example.capstone_1.repository.RefreshTokenRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 @Service
+@RequiredArgsConstructor
 public class AuthService {
-    @Autowired
-    MemberRepository memberRepository;
-    @Autowired
-    TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
 
-    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-    public ResponseDto<?> signUp(SignUpDto dto) {
-        String memberEmail = dto.getMemberEmail();
-        String memberPassword = dto.getMemberPassword();
-        String memberPasswordCheck = dto.getMemberPasswordCheck();
-
-
-        try{
-            if (memberRepository.existsById(memberEmail))
-                return ResponseDto.setFailed("Existed Email!");  //아이디 중복체크
-        }catch (Exception e){
-            return ResponseDto.setFailed("Data Base Error!");
+    @Transactional
+    public MemberResponseDto signup(MemberRequestDto memberRequestDto) {
+        if (memberRepository.existsByEmail(memberRequestDto.getEmail())) {
+            throw new RuntimeException("이미 가입되어 있는 유저입니다");
         }
 
-
-        //비밀번호가 서로 다른면 failed response 반환!
-        if (!memberPassword.equals(memberPasswordCheck)) //비밀번호와 비밀번호확인 서로 다르면 나오는 에러
-            return ResponseDto.setFailed("Password does not matched");
-
-        //UserEntity 생성
-        MemberEntity memberEntity = new MemberEntity(dto);
-
-        //비밀번호 암호화
-        String encodedPassword = passwordEncoder.encode(memberPassword);
-        memberEntity.setMemberPassword(encodedPassword);
-
-        try{
-            memberRepository.save(memberEntity);
-        }catch (Exception e){
-            return ResponseDto.setFailed("Data Base Error!");
-        }
-        return ResponseDto.setSucces("Sign Up Success!", null);
+        Member member = memberRequestDto.toMember(passwordEncoder);
+        return MemberResponseDto.of(memberRepository.save(member));
     }
 
+    @Transactional
+    public TokenDto login(LoginDto loginDto) { //로그인
+        // 1. Login ID/PW 를 기반으로 AuthenticationToken 생성
+        UsernamePasswordAuthenticationToken authenticationToken = loginDto.toAuthentication();
 
-    public ResponseDto<SignInResponseDto> signIn(SignInDto dto){
-        String memberEmail = dto.getMemberEmail();
-        String memberPassword = dto.getMemberPassword();
-        //String memberRole = dto.getMemberRole();
+        // 2. 실제로 검증 (사용자 비밀번호 체크) 이 이루어지는 부분
+        //    authenticate 메서드가 실행이 될 때 CustomUserDetailsService 에서 만들었던 loadUserByUsername 메서드가 실행됨
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
 
-        MemberEntity memberEntity = null;
-        try{
-            memberEntity = memberRepository.findByMemberEmail(memberEmail);
-            //잘못된 이메일
-            if(memberEntity == null)return ResponseDto.setFailed("Sign In Failed");
+        // 3. 인증 정보를 기반으로 JWT 토큰 생성
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
 
+        // 4. RefreshToken 저장
+        RefreshToken refreshToken = RefreshToken.builder()
+                .key(authentication.getName())
+                .value(tokenDto.getRefreshToken())
+                .build();
 
-            //if (memberRole.equals("ADMIN")){
-            //잘못된 패스워드
-            //      if (!memberPassword.equals(memberEntity.getMemberPassword())) {
-            //         return ResponseDto.setFailed("Sign In Failed");
-            //   }
+        refreshTokenRepository.save(refreshToken);
 
-            //}else{
-            if(!passwordEncoder.matches(memberPassword,memberEntity.getMemberPassword()))
-                return ResponseDto.setFailed("Sign In Failed");
-            //}
+        // 5. 토큰 발급
+        return tokenDto;
+    }
 
-
-        }catch (Exception error){
-            return ResponseDto.setFailed("Database Error");
+    @Transactional
+    public TokenDto reissue(TokenRequestDto tokenRequestDto) {
+        // 1. Refresh Token 검증
+        if (!tokenProvider.validateToken(tokenRequestDto.getRefreshToken())) {
+            throw new RuntimeException("Refresh Token 이 유효하지 않습니다.");
         }
 
-        memberEntity.setMemberPassword("");
+        // 2. Access Token 에서 Member ID 가져오기
+        Authentication authentication = tokenProvider.getAuthentication(tokenRequestDto.getAccessToken());
 
-        String token = tokenProvider.create(memberEmail);
-        int exprTime = 3600000;
+        // 3. 저장소에서 Member ID 를 기반으로 Refresh Token 값 가져옴
+        RefreshToken refreshToken = refreshTokenRepository.findByKey(authentication.getName())
+                .orElseThrow(() -> new RuntimeException("로그아웃 된 사용자입니다."));
 
-        SignInResponseDto signInResponseDto = new SignInResponseDto(token, exprTime, memberEntity);
-        return ResponseDto.setSucces("Sign In Success", signInResponseDto);
+        // 4. Refresh Token 일치하는지 검사
+        if (!refreshToken.getValue().equals(tokenRequestDto.getRefreshToken())) {
+            throw new RuntimeException("토큰의 유저 정보가 일치하지 않습니다.");
+        }
+
+        // 5. 새로운 토큰 생성
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+
+        // 6. 저장소 정보 업데이트
+        RefreshToken newRefreshToken = refreshToken.updateValue(tokenDto.getRefreshToken());
+        refreshTokenRepository.save(newRefreshToken);
+
+        // 토큰 발급
+        return tokenDto;
     }
 }
