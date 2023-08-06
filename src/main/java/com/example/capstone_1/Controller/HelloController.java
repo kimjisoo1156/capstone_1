1370ff0895ce25ab4c94c0a7a2f9a21a4af60a38
kimjisoo1156 <shfur2918@naver.com>
2023-08-06 18:33:15 +0900

    실제서버환경_TEST3

--- src/main/java/com/example/capstone_1/Controller/HelloController.java
+++ src/main/java/com/example/capstone_1/Controller/HelloController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hi";
+        return "Hi2";
     }
 }
