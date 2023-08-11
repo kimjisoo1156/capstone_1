2c7beea2c2665673fd3984544f50d0146b62f6d8
kimjisoo1156 <shfur2918@naver.com>
2023-08-11 16:53:19 +0900

    S3image2

---
+++ src/main/generated/com/example/capstone_1/domain/QFileEntity.java
@@ -0,0 +1,41 @@
+package com.example.capstone_1.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QFileEntity is a Querydsl query type for FileEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QFileEntity extends EntityPathBase<FileEntity> {
+
+    private static final long serialVersionUID = 1577189233L;
+
+    public static final QFileEntity fileEntity = new QFileEntity("fileEntity");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath s3Url = createString("s3Url");
+
+    public final StringPath title = createString("title");
+
+    public QFileEntity(String variable) {
+        super(FileEntity.class, forVariable(variable));
+    }
+
+    public QFileEntity(Path<? extends FileEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QFileEntity(PathMetadata metadata) {
+        super(FileEntity.class, metadata);
+    }
+
+}
+
