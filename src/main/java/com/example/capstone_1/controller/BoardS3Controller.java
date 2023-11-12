c69bc8e897d25c6e60507d5f77629462cfd44027
kimjisoo1156 <shfur2918@naver.com>
2023-11-12 19:55:09 +0900

    board_secret_read2

--- src/main/java/com/example/capstone_1/controller/BoardS3Controller.java
+++ src/main/java/com/example/capstone_1/controller/BoardS3Controller.java
@@ -135,7 +135,7 @@ public class BoardS3Controller {
         String secret = boardRepository.getSecretOfBoard(bno);
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        if (currentUser.getUsername().equals(writer) & secret.equals("0")||currentUser.getUsername().equals("darkest0722@gmail.com")){
+        if (currentUser.getUsername().equals(writer) || secret.equals("0")||currentUser.getUsername().equals("darkest0722@gmail.com")){
             BoardType enumBoardType = BoardType.valueOf(boardType); //문자열 enum으로
             Board_File_DTO boardWithImages = boardControllerService.getBoardWithImages(enumBoardType, bno);
             return ResponseEntity.ok(boardWithImages);
